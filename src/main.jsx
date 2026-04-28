69a8f444a5078a1f4e8f0a9b731ec8e592607984
Nathan Ariel Damasio Leão <nathanarieldamasioleao@gmail.com>
2026-04-28 17:43:09 -0300

    correção do P minusculo do Projeto no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom' 
-import Projeto1 from './pages/projeto1.jsx'
+import Projeto1 from './pages/Projeto1.jsx'
 
 const router = createBrowserRouter([
     {
