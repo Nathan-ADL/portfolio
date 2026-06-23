11c69dccc11d555f8140fbc1bf7c4fc55c0d5a7b
Nathan Ariel Damasio Leão <nathanarieldamasioleao@gmail.com>
2026-06-23 18:31:33 -0300

    fix: foto perfil

--- src/App.jsx
+++ src/App.jsx
@@ -159,7 +159,7 @@ export default function App() {
 
             {/* IMAGEM */}
             <div className="w-60 h-60 rounded-full overflow-hidden border-2">
-              <img src="/NathanAriel-pfp.png" className="w-full h-full object-cover" />
+              <img src="/NathanAriel-pfp.png" className="w-full h-full object-cover"/>
             </div>
           </div>
         </div>
